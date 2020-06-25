92811bc537ce72ec8602c53baf1644e0b69ffc18
Cindy <0126cloud@gmail.com>
2020-06-25 23:48:38 +0800

    0625 23:48 kenny

--- src/components/Course/Course.jsx
+++ src/components/Course/Course.jsx
@@ -1,138 +1,47 @@
 import React, { useState, useEffect } from 'react'
 
 import './course-style.css'
-import { Container, Row, Col, Card, Image } from 'react-bootstrap'
 
 import { withRouter } from 'react-router-dom'
 
 function Course(props) {
   return (
-    <Container className="container mb-2">
-      <Row>
-        <Col xs={12} md={4} className="d-flex">
-          <div className="item-img">
-            <a
-              href={`/life/courseDetail${props.linkUrl}?courseId=${props.courseId}`}
-            >
-              <img
-                thumbnail
-                style={{ width: '200px', height: '200px' }}
-                // onClick={() =>
-                //   props.history
-                //     .push
-                //     `/life/courseDetail${props.linkUrl}?courseId=${props.courseId}`
-                //     ()
-                // }
-                src={`/courses/${props.courseImg}`}
-                alt={props.courseImg}
-                onMouseOver={(e) =>
-                  (e.currentTarget.src = `/courses/${props.courseImg2}`)
-                }
-                onMouseOut={(e) =>
-                  (e.currentTarget.src = `/courses/${props.courseImg}`)
-                }
-              />
-            </a>
-            <div className="learnMore">どうぞ</div>
-          </div>
-          <div className="ml-2">
-            <Card
-              style={{ width: '35rem', height: '200px', marginLeft: '135px' }}
-            >
-              <Card.Body>
-                <Card.Title>
-                  <h2>{props.courseName}</h2>
-                </Card.Title>
-                <Card.Subtitle
-                  className="mt-1 d-flex"
-                  style={{ width: '70%', color: '#C0C0C0' }}
-                >
-                  課程簡介 : {props.courseDesc}
-                </Card.Subtitle>
+    <div className="course-card" value={props.courseId}>
+      <a
+        className="course-img"
+        href={`/life/courseDetail${props.linkUrl}?courseId=${props.courseId}`}
+      >
+        <img
+          src={`/courses/${props.courseImg}`}
+          alt={props.courseImg}
+          value={props.courseId}
+          onMouseOver={(e) =>
+            (e.currentTarget.src = `/courses/${props.courseImg2}`)
+          }
+          onMouseOut={(e) =>
+            (e.currentTarget.src = `/courses/${props.courseImg}`)
+          }
+        />
+        <div className="learnMore">どうぞ</div>
+      </a>
 
-                <Card.Subtitle
-                  className="mb-1 mt-4 d-flex"
-                  style={{ color: '#C0C0C0' }}
-                >
-                  人數上限：{props.courseQty}
-                </Card.Subtitle>
+      <div className="content">
+        <div className="course-content-left">
+          <h2 className="course-name">{props.courseName}</h2>
+          <p className="course-desc">{props.courseDesc}</p>
+          <p className="course-qty">人數上限：{props.courseQty}</p>
+        </div>
 
-                <div>
-                  <Card.Text className="item-price text-right cardText2">
-                    $ {props.coursePrice}
-                  </Card.Text>
+        <div className="course-right">
+          <h2 className="course-price">$ {props.coursePrice}</h2>
 
-                  <Card.Link>
-                    <button
-                      className="add-cart cardLink"
-                      onClick={props.handleClick}
-                      style={{
-                        position: 'absolute',
-                        bottom: '10px',
-                        right: '10px',
-                      }}
-                    >
-                      add to cart
-                      <i
-                        class="fas fa-shopping-cart"
-                        style={{ marginLeft: '8px' }}
-                      ></i>
-                    </button>
-                  </Card.Link>
-
-                  {/* <Card.Link>
-                    <button
-                      className="add-fav cardLink"
-                      onClick={props.handleWishListClick}
-                    >
-                      add to favtorite
-                    </button>
-                  </Card.Link> */}
-                </div>
-              </Card.Body>
-            </Card>
-          </div>
-        </Col>
-      </Row>
-    </Container>
-
-    // <div className="item-card" value={props.courseId}>
-    //   <div
-    //     className="item-img"
-    //     onClick={() =>
-    //       props.history.push(
-    //         `/life/courseDetail${props.linkUrl}?courseId=${props.courseId}`
-    //       )
-    //     }
-    //   >
-    //     <img
-    //       src={`/courses/${props.courseImg}`}
-    //       alt={props.courseImg}
-    //       value={props.courseId}
-    //       onMouseOver={(e) =>
-    //         (e.currentTarget.src = `/courses/${props.courseImg2}`)
-    //       }
-    //       onMouseOut={(e) =>
-    //         (e.currentTarget.src = `/courses/${props.courseImg}`)
-    //       }
-    //     />
-    //     <div className="learnMore">どうぞ</div>
-    //   </div>
-    //   <div className="item-content">
-    //     <div className="item-content-left">
-    //       <h3 className="item-name">{props.courseName}</h3>
-    //       <p className="item-description">{props.courseDesc}</p>
-    //       <p className="item-qty">人數上限：{props.courseQty}</p>
-    //     </div>
-    //     <div className="item-content-right">
-    //       <h3 className="item-price">$ {props.coursePrice}</h3>
-    //       <button className="add-cart" onClick={props.handleClick}>
-    //         add to cart
-    //       </button>
-    //       <button className="add-fav">add to favtorite</button>
-    //     </div>
-    //   </div>
-    // </div>
+          <button className="add-cart" onClick={props.handleClick}>
+            add to cart
+            <i class="fas fa-shopping-cart" style={{ marginLeft: '8px' }}></i>
+          </button>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,17 +1,6 @@
 import React from 'react'
 import { withRouter } from 'react-router-dom'
 
-import {
-  Table,
-  Container,
-  Row,
-  Col,
-  Card,
-  ListGroup,
-  Image,
-  Media,
-  Button,
-} from 'react-bootstrap'
 import './Item/item-style.css'
 
 function Item(props) {
@@ -29,103 +18,45 @@ function Item(props) {
   // console.log(props)
 
   return (
-    <Container className="container mb-2">
-      <Row>
-        <Col xs={12} md={4} className="d-flex">
+    <>
+      <div className="container mb-2 outside">
+        <div className="img">
           <a
             href={`/mall/itemDetail${props.linkUrl}/categoryId=${props.categoryId}?itemId=${props.itemId}`}
           >
             <img
-              thumbnail
-              // onClick={() =>
-              //   props.history.push(
-              //     `/mall/itemDetail${props.linkUrl}/categoryId=${props.categoryId}?itemId=${props.itemId}`
-              //   )
-              // }
               src={`/items/${props.itemImg}`}
               alt={props.itemImg}
-              style={{ height: '200px', width: '200px' }}
+              style={{ height: '250px', width: '250px' }}
+              className="item-pic"
             />
           </a>
-
-          <div className="ml-2">
-            <Card style={{ width: '35rem', height: '200px' }}>
-              <Card.Body>
-                <Card.Title>
-                  <h2>{props.itemName}</h2>
-                </Card.Title>
-                <Card.Subtitle className="mb-2 mt-2 text-muted d-flex">
-                  商品介紹 : {props.itemDescription}
-                </Card.Subtitle>
-                <div>
-                  <Card.Text className="item-price text-right cardText2">
-                    $ {props.itemPrice}
-                  </Card.Text>
-
-                  <Card.Link>
-                    <button
-                      className="add-cart cardLink"
-                      onClick={props.handleClick}
-                    >
-                      add to cart
-                      <i
-                        class="fas fa-shopping-cart"
-                        style={{ marginLeft: '8px' }}
-                      ></i>
-                    </button>
-                  </Card.Link>
-                  <Card.Link>
-                    <button
-                      className="add-fav cardLink"
-                      onClick={props.handleWishListClick}
-                    >
-                      add to favtorite
-                    </button>
-                  </Card.Link>
-                </div>
-              </Card.Body>
-            </Card>
-          </div>
-
-          {/* <div
-        className="item-content"
-        value={props.itemId}
-        style={{
-          background: 'white',
-          border: '1px solid white',
-          marginBottom: '5px',
-          justifyContent: 'center',
-        }}
-      >
-        <div className="item-img">
-          <img
-            src={`/items/${props.itemImg}`}
-            alt={props.itemImg}
-            value={props.itemId}
-            style={{ width: '100%', height: '150%' }}
-          />
-          <div className="learnMore">ディテール</div>
         </div>
-        <div className="item-content">
-          <div className="item-content-left"> */}
-          {/* <h3 className="item-name">{props.itemName}</h3> */}
-          {/* <p className="item-description">{props.itemDescription}</p> */}
-          {/* </div>
-          <div className="item-content-right"> */}
-          {/* <h3 className="item-price">$ {props.itemPrice}</h3> */}
-          {/* <button className="add-cart" onClick={props.handleClick}>
+
+        <div className="item-desc">
+          <div className="title-desc">
+            <h2 className="name">{props.itemName}</h2>
+            商品介紹 : {props.itemDescription}
+          </div>
+          <div className="item-price">$ {props.itemPrice}</div>
+          <div className="add-btn">
+            <button
+              className="item-add-cart cardLink btn-left"
+              onClick={props.handleClick}
+            >
               add to cart
               <i class="fas fa-shopping-cart" style={{ marginLeft: '8px' }}></i>
-            </button> */}
-          {/* <button className="add-fav" onClick={props.handleWishListClick}>
+            </button>
+            <button
+              className="add-fav cardLink"
+              onClick={props.handleWishListClick}
+            >
               add to favtorite
-            </button> */}
-          {/* </div>
+            </button>
+          </div>
         </div>
-      </div> */}
-        </Col>
-      </Row>
-    </Container>
+      </div>
+    </>
   )
 }
 
